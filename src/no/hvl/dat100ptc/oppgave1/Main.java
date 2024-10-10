62c5b0704bcd5e96dbfb4c352fee1899b9d99940
h669845 <patrik.thormodsen@gmail.com>
2024-10-10 10:20:11 +0200

    Update Main.java

--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -5,6 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		
 		// TODO
+		GPSPoint gpspoint = new GPSPoint(1, 2.0, 3.0, 4.0);
+		
+		System.out.println("Tid: " + gpspoint.getTime());
+		
+		gpspoint.setTime(2);
+		
+		System.out.println(gpspoint.toString());
 		
 	}
 
